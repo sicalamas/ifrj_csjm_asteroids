1451ef5cb0c4588e4890bff00cd3c484c608ca94
sicalamas <50429475+sicalamas@users.noreply.github.com>
2019-05-08 21:30:01 -0300

    Add files via upload

---
+++ Entity.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    // Entity -> Classe base para todos os objetos do jogo
+    class Entity
+    {
+        // Atributos
+        protected Vector2 position; // Posição 2D
+        protected Vector2 velocity; // Velocidade & Direção 2D
+        protected float angle; // Angulo do desenho
+        protected float scale; // Escala do desenho
+
+        // Métodos
+
+        // init() -> Inicializa variáveis
+        public virtual void init() { }
+
+        // load(cM) -> Carrega imagens e sons
+        // cM -> Gerenciador de arquivos (Biblioteca)
+        public virtual void load(ContentManager cM) { }
+
+        // unload() -> Descarrega o objeto da memória
+        public virtual void unload() { }
+
+        // update(gT) -> Lógica do objeto (teclado, mouse, algoritmos)
+        // gT -> Controle do tempo (biblioteca)
+        public virtual void update(GameTime gT) { }
+
+        // draw(sB) -> Funções de desenho do jogo
+        // sB -> Objeto desenhador da biblioteca ("css" do jogo)
+        public virtual void draw(SpriteBatch sB) { }
+
+    }
+}
---
+++ Spaceship.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    // Nave espacial controlada pelo jogador
+    class Spaceship : Entity
+    {
+        // Atributos
+        ///CircleF collider; // Area para teste de colisão
+        bool isAccelerating; // Teste: Está acelerando?
+        float accelIndex; // Índice de aceleração
+        float dragIndex; // Índice de desaceleração
+
+        // atributo temporário para testes
+        Texture2D textura;
+
+        // Métodos
+        public override void init()
+        {
+            // Inicializa os atributos da superclasse
+            position = new Vector2(100, 100);
+            velocity = new Vector2(0, 0);
+            angle = MathHelper.ToRadians(270); // = 270 graus convertido para radianos
+            scale = 2.0f;
+            // Inicializa atributos próprios
+            isAccelerating = false;
+            accelIndex = 1.0f;
+            dragIndex = 0.99f;
+            base.init();
+        }
+
+        public override void load(ContentManager cM)
+        {
+            // Carrega textura na memória do objeto
+            textura = cM.Load<Texture2D>("SpriteSheet");
+            base.load(cM);
+        }
+
+        public override void unload()
+        {
+            textura.Dispose();
+            base.unload();
+        }
+
+        public override void update(GameTime gT)
+        {
+            base.update(gT);
+        }
+
+        public override void draw(SpriteBatch sB)
+        {
+            sB.Draw(textura,
+                position,
+                new Rectangle(0, 0, 16, 16),
+                Color.White,
+                angle,
+                new Vector2(8, 8),
+                scale,
+                SpriteEffects.None,
+                0);
+            base.draw(sB);
+        }
+    }
+}
